0b1582cbb16ac89af718a8af050095ad7fdb575f
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2016-10-13 15:27:36 +0200

    Update optimize.sh

--- optimize.sh
+++ optimize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo Optimizing mysql table...
-mysqlcheck ${MYSQLCHECK_OPTS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USER} -p${MYSQL_PASS}
+mysqlcheck ${MYSQLCHECK_OPTS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USERNAME} -p${MYSQL_PASSWORD}
 echo Optimize process done
