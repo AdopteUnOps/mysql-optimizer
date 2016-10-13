e6c51513113b6ddceafc263596925386af34a9f1
Jean-Louis Boudart <jeanlouis.boudart@gmail.com>
2016-10-13 16:35:02 +0200

    add some info about duration

--- optimize.sh
+++ optimize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo Optimizing mysql table...
-mysqlcheck ${MYSQLCHECK_OPTS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USERNAME} -p${MYSQL_PASSWORD}
-echo Optimize process done
+echo Starting optimizing mysql tables on $(date)...
+time mysqlcheck ${MYSQLCHECK_OPTS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USERNAME} -p${MYSQL_PASSWORD}
+echo Optimize process finished on $(date)
